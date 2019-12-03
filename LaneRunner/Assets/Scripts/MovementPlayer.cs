d2955f973e20b08142e535b07a6b63036f83fef7
Itamar <itamar@live.nl>
2019-12-03 17:24:29 +0100

    Movement, lanes, and some colours

---
+++ LaneRunner/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementPlayer : MonoBehaviour
+{
+    void Update()
+    {
+        //movement
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+
+        //clamping the player
+        var pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, -1.0f, 1.0f);
+        transform.position = pos;
+    }
+}
