8b6b1d431e6b0492bef619e71a4a8443232518b8
Itamar <itamar@live.nl>
2019-12-03 18:58:04 +0100

    adding basic enemy, lives and global manager script - setup for widget for lives and points and repeating enemies spawning

--- LaneRunner/Assets/Scripts/ManagerGlobal.cs
+++ LaneRunner/Assets/Scripts/ManagerGlobal.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Points : MonoBehaviour
+public class ManagerGlobal : MonoBehaviour
 {
     public float PointsCounter = 0;
+    public float Lives = 3;
 
-    //collision
+    //collision and adding points
     void OnTriggerEnter(Collider otherobject)
     {
         if(otherobject.gameObject.tag == "Points")
         {
             PointsCounter++;
+            Destroy(otherobject.gameObject);
+        }
+        if(otherobject.gameObject.tag == "EnemySimple")
+        {
+            Lives--;
             Destroy(otherobject.gameObject);
         }
     }
---
+++ LaneRunner/Assets/Scripts/PointsManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsManager : MonoBehaviour
+{
+    public GameObject Points;
+
+    //invoke spawning lanes every x seconds on x lane
+    void Start()
+    {
+        InvokeRepeating("SpawnPointsMiddle", 0, 10);
+        InvokeRepeating("SpawnPointsLeft", 2, 10);
+        InvokeRepeating("SpawnPointsRight", 4, 10);
+    }
+
+    //spawning le points
+    void SpawnPointsMiddle()
+    {
+        Instantiate(Points, new Vector3(0f, 0.2f, 30f), Quaternion.identity);
+        Instantiate(Points, new Vector3(0f, 0.2f, 30.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(0f, 0.2f, 31f), Quaternion.identity);
+        Instantiate(Points, new Vector3(0f, 0.2f, 31.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(0f, 0.2f, 32f), Quaternion.identity);
+    }
+
+    void SpawnPointsLeft()
+    {
+        Instantiate(Points, new Vector3(1f, 0.2f, 30f), Quaternion.identity);
+        Instantiate(Points, new Vector3(1f, 0.2f, 30.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(1f, 0.2f, 31f), Quaternion.identity);
+        Instantiate(Points, new Vector3(1f, 0.2f, 31.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(1f, 0.2f, 32f), Quaternion.identity);
+    }
+
+    void SpawnPointsRight()
+    {
+        Instantiate(Points, new Vector3(-1f, 0.2f, 30f), Quaternion.identity);
+        Instantiate(Points, new Vector3(-1f, 0.2f, 30.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(-1f, 0.2f, 31f), Quaternion.identity);
+        Instantiate(Points, new Vector3(-1f, 0.2f, 31.5f), Quaternion.identity);
+        Instantiate(Points, new Vector3(-1f, 0.2f, 32f), Quaternion.identity);
+    }
+}
