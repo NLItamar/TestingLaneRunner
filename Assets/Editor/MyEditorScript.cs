bd3857a5d0dc7cdb4f938e4cc07cbdac6ac547de
Itamar <itamar@live.nl>
2019-12-11 17:29:22 +0100

    some changes to jenkins and stuff

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,11 @@
+﻿// C# example
+using UnityEditor;
+
+class MyEditorScript
+{
+    static void PerformBuild()
+    {
+        string[] scenes = {"Assets/Scenes/LaneRunner.unity" };
+        BuildPipeline.BuildPlayer(scenes, "D:\testing2019\builds", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+}
