4a6750e4f72a0d978d3005ae52123a7523bf3618
Itamar <itamar@live.nl>
2019-12-12 14:29:40 +0100

    changes

--- Assets/Jenkins/Editor/MyEditorScript.cs
+++ Assets/Jenkins/Editor/MyEditorScript.cs
@@ -5,7 +5,7 @@ class MyEditorScript
 {
     static void PerformBuild()
     {
-        string[] scenes = {"Assets/Scenes/LaneRunner.unity" };
+        string[] scenes = {"Assets/Scenes/TestingLaneRunner.unity" };
         BuildPipeline.BuildPlayer(scenes, "D:/testing2019/builds/game.exe", BuildTarget.StandaloneWindows64, BuildOptions.CompressWithLz4);
     }
 }
